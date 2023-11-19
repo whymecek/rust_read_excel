7382383d049c47e216e7aafcfcea08714e0caeb5
brian <brianmr31@gmail.com>
2023-11-19 07:38:06 +0700

    add sleep to show memory used

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
 use calamine::{ Reader, open_workbook_auto };
-use std::time::SystemTime;
-use std::collections::HashMap; 
+// use std::collections::HashMap; 
+use ahash::AHashMap;
+use std::{thread, time::Duration, time::SystemTime};
+
 
 fn main() {
     let start = SystemTime::now();
     
-    let path = "./Electric_Vehicle_Population_Data2.xlsx";
-    // let path = "./Electric_Vehicle_Population_Data3.xlsx";
+    // let path = "./Electric_Vehicle_Population_Data2.xlsx";
+    let path = "./Electric_Vehicle_Population_Data3.xlsx";
     let mut workbook = open_workbook_auto(path).expect("Cannot open file");
     println!("Execution Read {:?} ", ( SystemTime::now().duration_since(start).unwrap() ) );
 
-    let mut data: HashMap<i32, Vec<String>> = HashMap::new();
+    // let mut data: HashMap<i32, Vec<String>> = HashMap::new();
+    let mut data: AHashMap<i32, Vec<String>> = AHashMap::new();
     let mut i: i32 = 0;
+    // let mut tmp :Vec<String> = Vec::new();
     if let Some(Ok(r)) = workbook.worksheet_range( "Sheet1" ) {
         for row in r.rows() {
-            let mut tmp :Vec<String>  = Vec::new();
+            data.insert(i, Vec::new() );
             for x in 0..row.len() {
-                tmp.push( row[x].to_string() );
+		data.get_mut(&i).expect("DATA NOT FOUND").push( row[x].to_string() );
+                // tmp.push( row[x].to_string() );
             }
-            data.insert(i, tmp);
+	    // tmp.clear();
             i+=1;
         }
     }
     println!("Count Data  {:?} ", data.len() );
     println!("Execution End  {:?} ", ( SystemTime::now().duration_since(start).unwrap().as_millis() ) );
+
+    thread::sleep(Duration::from_millis(4000));
 }
